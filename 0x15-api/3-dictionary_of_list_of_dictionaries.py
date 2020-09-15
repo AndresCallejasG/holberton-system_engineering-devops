e22fa7cc6e224d41a8743fdfbc9e376b34a4830f
andrescallejasg <andres.callejasg@gmail.com>
2020-09-15 01:20:38 +0000

    Records all tasks from all employees

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" using a REST API, Records all tasks from all employees in a json
+"""
+import json
+import requests
+
+if __name__ == "__main__":
+    """ script
+    """
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get('{}users/'.format(url)).json()
+    tasks = requests.get('{}todos'.format(url)).json()
+
+    json_str = {}
+    for usr in users:
+        id = usr.get('id')
+        tasks_list = []
+        for elem in tasks:
+            if id == elem.get('userId'):
+                data = {
+                    'task': elem.get('title'),
+                    'completed': elem.get('completed'),
+                    'username': usr.get('username')
+                }
+                print("yes")
+                tasks_list.append(data)
+        json_str.update({str(id): tasks_list})
+    json_file = "todo_all_employees.json"
+    with open(json_file, 'w') as f:
+        json.dump(json_str, f)
