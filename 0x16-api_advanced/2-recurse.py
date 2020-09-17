2d3b50fe29c01ae385b29f5dc279fbbf822a402c
andrescallejasg <andres.callejasg@gmail.com>
2020-09-17 23:36:36 +0000

    all posts

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" queries the Reddit API and returns a list containing
+    the titles of all hot articles for a given subreddit
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """returns all the titles from the hottest posts
+    """
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
+    if after:
+        url += "?after=" + after
+    user_agent = {"User-Agent": "script:subs:v1 (by andrescallejasg)"}
+    data = requests.get(url, headers=user_agent).json()
+    try:
+        posts = data['data']['children']
+    except KeyError:
+        print("None")
+        return
+
+    for post in posts:
+        hot_list.append(post['data']['title'])
+    af = data['data']['after']
+    if af:
+        return recurse(subreddit, hot_list, af)
+    return hot_list
