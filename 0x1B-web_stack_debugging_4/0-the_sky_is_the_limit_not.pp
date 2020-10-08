3bc3797eb3ea10a6deb435782a40f9b3fed695ef
andrescallejasg <andres.callejasg@gmail.com>
2020-10-08 15:53:59 -0500

    server version - Setting ULIMIT

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Setting ULIMIT
 exec { 'fix-for-nginx':
-  command => 'sed -i s/-n 15/-n 800/ /etc/default/nginx && sudo service nginx restart',
+  command => 'sed -i s/15/800/ /etc/default/nginx && service nginx restart',
   path    => '/bin/:/usr/bin/',
 }
